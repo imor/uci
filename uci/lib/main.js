12ac997482fb6c1a7fb89c1fc84e27b7b47591d0
Raminder Singh <romi_ssk@yahoo.co.in>
2013-05-16 12:25:29 +0530

    beautified code

--- uci/lib/main.js
+++ uci/lib/main.js
@@ -5,115 +5,118 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 var Chess = require('./chess.js').Chess;
 
-var Engine = function() {
-	var self = this;
-	////TODO:Remove hardcoded executable name
-	var engine = spawn(path.join(__dirname, '../engines/stockfish/stockfish-3-32-ja.exe'));
+var Engine = function () {
+    var self = this;
+    ////TODO:Remove hardcoded executable name
+    var engine = spawn(path.join(__dirname, '../engines/stockfish/stockfish-3-32-ja.exe'));
 
-	engine.on('close', function (code) {
-	});
+    engine.on('close', function (code) {});
 
-	function checker(data, ok_response) {
-		var str = data.toString().replace('\r\n', '\n').replace('\r', '\n');
-		var arr = str.split(/\n/);
-		for (var i = 0;i < arr.length;i++) {
-			if (arr[i].replace('\r', '') == ok_response) {
-				return true;
-			}
-		}
-		return false;
-	}
+    function checker(data, ok_response) {
+        var str = data.toString().replace('\r\n', '\n').replace('\r', '\n');
+        var arr = str.split(/\n/);
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].replace('\r', '') == ok_response) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	function run_engine_command(commands, ok_response, result_checker) {
-		result_checker = result_checker ? result_checker : checker;
-		var deferred = Q.defer();
-		var engine_stdout_listener = function(data) {
-			var result = result_checker(data, ok_response);
-			if (result) {
-				clearTimeout(timerId);
-				engine.stdout.removeListener('data', engine_stdout_listener);
-				deferred.resolve(result);
-			}
-		};
-		engine.stdout.on('data', engine_stdout_listener);
-		for (var i = 0;i < commands.length;++i) {
-			engine.stdin.write(commands[i] + '\n');
-		}
+    function run_engine_command(commands, ok_response, result_checker) {
+        result_checker = result_checker ? result_checker : checker;
+        var deferred = Q.defer();
+        var engine_stdout_listener = function (data) {
+            var result = result_checker(data, ok_response);
+            if (result) {
+                clearTimeout(timerId);
+                engine.stdout.removeListener('data', engine_stdout_listener);
+                deferred.resolve(result);
+            }
+        };
+        engine.stdout.on('data', engine_stdout_listener);
+        for (var i = 0; i < commands.length; ++i) {
+            engine.stdin.write(commands[i] + '\n');
+        }
 
-		var timeout = 5000;
-		var timerId = setTimeout(function() {
-			engine.stdout.removeListener('data', engine_stdout_listener);
-			deferred.reject(new Error("Didn't receive result within " + timeout + " millisecs"));
-		}, timeout);
-		return deferred.promise;
-	}
+        var timeout = 5000;
+        var timerId = setTimeout(function () {
+            engine.stdout.removeListener('data', engine_stdout_listener);
+            deferred.reject(new Error("Didn't receive result within " + timeout + " millisecs"));
+        }, timeout);
+        return deferred.promise;
+    }
 
-	function delay(ms) {
-		var deferred = Q.defer();
-		setTimeout(deferred.resolve, ms);
-		
-		return deferred.promise;
-	}
+    function delay(ms) {
+        var deferred = Q.defer();
+        setTimeout(deferred.resolve, ms);
 
-	run_engine_command(['uci'], 'uciok').then(function() {
-	self.emit('ready');});
+        return deferred.promise;
+    }
 
-	function moveExtractor(data, ok_response) {
-		var str = data.toString().replace('\r\n', '\n').replace('\r', '\n');
-		var arr = str.split(/\n/);
-		for (var i = 0;i < arr.length;i++) {
-			var line = arr[i].replace('\r', '');
-			if (line.substring(0, ok_response.length) === ok_response) {
-				var moveRegex = /bestmove (.*?) /g;
-				var match = moveRegex.exec(line);
-				if (match) {
-					return convertToMoveObject(match[1]);
-				}
-				else {
-					throw new Error('Invalid format of bestmove. Expected "bestmove <move>". Returned "' + line +'"');
-				}
-			}
-		}
-		return false;
-	}
+    run_engine_command(['uci'], 'uciok').then(function () {
+        self.emit('ready');
+    });
 
-	function convertToMoveObject(moveStr) {
-		var result = {};
-		result.from = moveStr.substring(0, 2);
-		result.to = moveStr.substring(2, 4);
-		if (moveStr.length > 4) {
-			result.promotion = moveStr.substring(5);
-		}
-		return result;
-	}
+    function moveExtractor(data, ok_response) {
+        var str = data.toString().replace('\r\n', '\n').replace('\r', '\n');
+        var arr = str.split(/\n/);
+        for (var i = 0; i < arr.length; i++) {
+            var line = arr[i].replace('\r', '');
+            if (line.substring(0, ok_response.length) === ok_response) {
+                var moveRegex = /bestmove (.*?) /g;
+                var match = moveRegex.exec(line);
+                if (match) {
+                    return convertToMoveObject(match[1]);
+                } else {
+                    throw new Error('Invalid format of bestmove. Expected "bestmove <move>". Returned "' + line + '"');
+                }
+            }
+        }
+        return false;
+    }
 
-	self.move = function(move, whiteMillisRemaining, blackMillisRemaining) {
-		var validMove = self.chess.move(convertToMoveObject(move));
-		if (validMove === null) {
-			throw new Error('Invalid move ' + move);
-		}
-		run_engine_command(['position fen ' + self.chess.fen(), 'isready'], 'readyok').then(function() {
-		return run_engine_command(createGoCommand(), 'bestmove', moveExtractor);}).then(function(move) {
-			self.chess.move(move);
-			self.emit('moved', move);});
-	};
+    function convertToMoveObject(moveStr) {
+        var result = {};
+        result.from = moveStr.substring(0, 2);
+        result.to = moveStr.substring(2, 4);
+        if (moveStr.length > 4) {
+            result.promotion = moveStr.substring(5);
+        }
+        return result;
+    }
 
-	function createGoCommand(whiteMillisRemaining, blackMillisRemaining) {
-		return ['go wtime ' + whiteMillisRemaining + ' btime ' + blackMillisRemaining];
-	}
+    self.move = function (move, whiteMillisRemaining, blackMillisRemaining) {
+        var validMove = self.chess.move(convertToMoveObject(move));
+        if (validMove === null) {
+            throw new Error('Invalid move ' + move);
+        }
+        run_engine_command(['position fen ' + self.chess.fen(), 'isready'], 'readyok').then(function () {
+            return run_engine_command(createGoCommand(), 'bestmove', moveExtractor);
+        }).then(function (move) {
+            self.chess.move(move);
+            self.emit('moved', move);
+        });
+    };
 
-	self.startNewGame = function(engineSide, whiteMillisRemaining, blackMillisRemaining) {
-		self.chess = new Chess();
-		run_engine_command(['ucinewgame', 'isready'], 'readyok').then(function() {
-		return run_engine_command(['position startpos', 'isready'], 'readyok');}).then(function() {
-			self.emit('newGameReady');
-			if (engineSide === 'w') {
-				run_engine_command(createGoCommand(whiteMillisRemaining, blackMillisRemaining), 'bestmove', moveExtractor).then(function(move) {
-				self.chess.move(move);
-				self.emit('moved', move);});
-			}
-		});
-	};
+    function createGoCommand(whiteMillisRemaining, blackMillisRemaining) {
+        return ['go wtime ' + whiteMillisRemaining + ' btime ' + blackMillisRemaining];
+    }
+
+    self.startNewGame = function (engineSide, whiteMillisRemaining, blackMillisRemaining) {
+        self.chess = new Chess();
+        run_engine_command(['ucinewgame', 'isready'], 'readyok').then(function () {
+            return run_engine_command(['position startpos', 'isready'], 'readyok');
+        }).then(function () {
+            self.emit('newGameReady');
+            if (engineSide === 'w') {
+                run_engine_command(createGoCommand(whiteMillisRemaining, blackMillisRemaining), 'bestmove', moveExtractor).then(function (move) {
+                    self.chess.move(move);
+                    self.emit('moved', move);
+                });
+            }
+        });
+    };
 };
 util.inherits(Engine, events.EventEmitter);
 exports.Engine = Engine;
--- ucidriver.js
+++ ucidriver.js
@@ -8,34 +8,33 @@ var whiteMillisRemaining = gameTime * 60 * 1000;
 var blackMillisRemaining = whiteMillisRemaining;
 
 var uci = new Engine();
-uci.on('ready', function() {
-	console.log('Engine ready');
+uci.on('ready', function () {
+    console.log('Engine ready');
 }).on('newGameReady', function () {
     lastTickTime = new Date();
     clock = setInterval(updateRemainingTimes, 250);
-	var stdin = process.openStdin();
-	stdin.on('data', function(move) {
+    var stdin = process.openStdin();
+    stdin.on('data', function (move) {
         turn = turn === 'w' ? 'b' : 'w';
-		uci.move(move.toString().replace('\r\n', ''), whiteMillisRemaining, blackMillisRemaining);
-	});
+        uci.move(move.toString().replace('\r\n', ''), whiteMillisRemaining, blackMillisRemaining);
+    });
 }).on('moved', function (move) {
     turn = turn === 'w' ? 'b' : 'w';
-	console.log('Engine moved ' + move.from + move.to);
+    console.log('Engine moved ' + move.from + move.to);
 });
 uci.startNewGame('b', whiteMillisRemaining, blackMillisRemaining);
 
 function updateRemainingTimes() {
     var now = new Date();
     var diff = now - lastTickTime;
-	var millisToUpdate;
-	if (turn === 'w') {
-		millisToUpdate = whiteMillisRemaining = whiteMillisRemaining - diff;
-		console.log('White remaing time ' + whiteMillisRemaining / 1000);
-	}
-	else {
-		millisToUpdate = blackMillisRemaining = blackMillisRemaining - diff;
-		console.log('Black remaing time ' + blackMillisRemaining / 1000);
-	}
+    var millisToUpdate;
+    if (turn === 'w') {
+        millisToUpdate = whiteMillisRemaining = whiteMillisRemaining - diff;
+        console.log('White remaing time ' + whiteMillisRemaining / 1000);
+    } else {
+        millisToUpdate = blackMillisRemaining = blackMillisRemaining - diff;
+        console.log('Black remaing time ' + blackMillisRemaining / 1000);
+    }
     if (millisToUpdate <= 0.0) {
         clearInterval(clock);
         console.log((turn === 'w' ? "White's" : "Black's") + ' time is up');
