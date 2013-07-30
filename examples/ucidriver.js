a253d17ae0ab3d70d89b4b2e834eb0285d3d4031
Raminder Singh <romi_ssk@yahoo.co.in>
2013-07-30 22:18:34 +0530

    Renamed event newGameReady to newgame
    Also improved the logging in the ucidriver example

--- examples/ucidriver.js
+++ examples/ucidriver.js
@@ -1,21 +1,23 @@
 var Engine = require('uci').Engine;
-
 var uci = new Engine();
+
 uci.on('ready', function () {
     console.log('Engine ready');
-}).on('newGameReady', function () {
+}).on('newgame', function () {
     var stdin = process.openStdin();
     stdin.on('data', function (move) {
         uci.move(move.toString().replace('\r\n', ''));
     });
 }).on('moved', function (move) {
-    console.log('Engine moved ' + move.from + move.to);
+    console.log('Engine moved ' + move.from + move.to +
+		(move.promotion ? move.promotion : ''));
 }).on('error', function (message) {
-    console.log(message);
+    console.log('Error:' + message);
 }).on('exit', function (message) {
-    console.log(message);
+    console.log('Exiting:' + message);
 }).on('gameends', function (result, reason) {
     console.log('Game ends with result ' + result + ' because ' + reason);
 });
 
-uci.startNewGame('b', 1);
+//Start a new 10 minute game with engine as black
+uci.startNewGame('b', 10);
