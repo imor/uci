fc41b3091f9623aa692401a5aeceb2213f3f313e
Raminder Singh <romi_ssk@yahoo.co.in>
2014-01-03 16:45:31 +0530

    Fixed exception in makeExecutable.js in windows

--- engines/makeExecutable.js
+++ engines/makeExecutable.js
@@ -8,12 +8,11 @@ process.chdir(path.normalize(process.cwd() + '/engines/'));
 
 if (os.platform() === 'linux') {
     var command = spawn('chmod', ['+x', path.normalize(__dirname + '/stockfish/stockfish-linux')]);
+    command.on('error', function (error) {
+        console.log(error);
+    });
 } else if (os.platform() === 'win32') {
     //Do nothing
 } if (os.platform() === 'darwin') {
     //TODO:Test on Mac
 }
-
-command.on('error', function (error) {
-        console.log(error);
-});
